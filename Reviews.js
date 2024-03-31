e71b5f2081438545bb51c411d09c0ef75fa85fc5
ivanc1218 <ivan.contreras@ucdenver.edu>
2024-03-31 11:28:36 -0600

    Updated server, added reviews, readded code from HW3

--- Reviews.js
+++ Reviews.js
@@ -3,10 +3,12 @@ var Schema = mongoose.Schema;
 
 mongoose.connect(process.env.DB);
 
-// Movie schema
-var ReviewSchema = new Schema({
-
-});
+const ReviewSchema = new mongoose.Schema({
+    movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' },
+    username: String,
+    content: String,
+    rating: { type: Number, min: 0, max: 5 }
+  });
 
 // return the model
 module.exports = mongoose.model('Review', ReviewSchema);
